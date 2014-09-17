c34a717b124bb1630a3187a64311009561cebb2a
gma <gary.ma@csgi.com>
2014-09-17 07:51:26 +0200

    inventory with path parameter

--- src/rest/dao/Schema.java
+++ src/rest/dao/Schema.java
@@ -41,4 +41,36 @@ public class Schema extends OracleDB{
 		
 		return json;
 	}
+	
+	public JSONArray queryReturnBrandItemNumber(String brand, int itemNumber) throws Exception{
+		PreparedStatement query = null;
+		Connection conn = null;
+		
+		ToJSON converter = new ToJSON();
+		JSONArray json = new JSONArray();
+		
+		try{
+			conn = oraclePcPartsConnection();
+			query = conn.prepareStatement("select PC_PARTS_PK, PC_PARTS_TITLE, "
+					+ "PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC "
+					+ "from PC_PARTS where UPPER(PC_PARTS_MAKER) = ? and PC_PARTS_CODE=?");
+			query.setString(1,  brand.toUpperCase());
+			query.setInt(2,	itemNumber);
+			ResultSet rs = query.executeQuery();
+			
+			json = converter.toJSONArray(rs);
+			query.close();
+		}catch(SQLException sqlError){
+			sqlError.printStackTrace();
+			return json;			
+		}catch(Exception e){
+			e.printStackTrace();
+			return json;
+		}finally{
+			if(conn != null)
+				conn.close();
+		}
+		
+		return json;
+	}
 }
--- src/rest/inventory/V2_inventory.java
+++ src/rest/inventory/V2_inventory.java
@@ -2,6 +2,7 @@ package rest.inventory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -25,7 +26,7 @@ public class V2_inventory {
 		
 		try{
 			if(brand == null)
-				return Response.status(400).entity("Error: please specify the value for the search").build();
+				return Response.status(400).entity("Error : please specify brand for this search").build();
 			
 			Schema sm = new Schema();
 			json = sm.queryReturnBrandParts(brand);
@@ -37,6 +38,42 @@ public class V2_inventory {
 			return Response.status(500).entity("Server was not able to process your request").build();
 		}
 		
+		return Response.ok(returnString).build();
+	}
+	
+	@Path("/{brand}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnBrand(@PathParam("brand") String brand) throws Exception{
+		String returnString = null;
+		JSONArray json = new JSONArray();
+		try{
+			Schema sm = new Schema();
+			json = sm.queryReturnBrandParts(brand);
+			returnString = json.toString();					
+		}catch(Exception e){
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		return Response.ok(returnString).build();
+	}
+	
+	@Path("/{brand}/{itemNumber}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnBrandItemNumber(
+			@PathParam("brand") String brand,
+			@PathParam("itemNumber") int itemNumber)throws Exception{
+		String returnString = null;
+		JSONArray json = new JSONArray();
+		try{
+			Schema sm = new Schema();
+			json = sm.queryReturnBrandItemNumber(brand, itemNumber);
+			returnString = json.toString();					
+		}catch(Exception e){
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
 		return Response.ok(returnString).build();
 	}
 }
