356324619eb1b334b13073f937fd47b07573caa1
gma <gary.ma@csgi.com>
2014-09-18 14:28:09 +0200

    objectmapper to insert data into database

--- src/rest/dao/Schema.java
+++ src/rest/dao/Schema.java
@@ -10,7 +10,59 @@ import org.codehaus.jettison.json.JSONArray;
 import util.ToJSON;
 
 public class Schema extends OracleDB{
+	
+	private int getNextValue() throws Exception{
+		PreparedStatement query = null;
+		Connection conn = null;
+		try{
+			conn = oraclePcPartsConnection();
+			query = conn.prepareStatement("select max(PC_PARTS_PK) from PC_PARTS");
+			ResultSet rs = query.executeQuery();
+			if(rs.next()){
+				return rs.getInt(1) + 1;
+			}else{
+				return 1;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			return 0;
+		}finally{
+			if(conn != null)
+				conn.close();
+		}
+	}
 
+	
+	public int insertIntoPC_PARTS(String PC_PARTS_TITLE, String PC_PARTS_CODE, 
+			String PC_PARTS_MAKER, String PC_PARTS_AVAIL,String PC_PARTS_DESC)
+					throws Exception{
+		PreparedStatement query = null;
+		Connection conn = null;
+		
+		try{
+			conn = oraclePcPartsConnection();
+			query = conn.prepareStatement("insert into PC_PARTS (PC_PARTS_PK, PC_PARTS_TITLE, "
+					+ "PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC) "
+					+ "VALUES(?,?,?,?,?,?) ");
+			query.setInt(1, getNextValue());
+			query.setString(2, PC_PARTS_TITLE);
+			query.setString(3, PC_PARTS_CODE);
+			query.setString(4, PC_PARTS_MAKER);
+			int avilInt = Integer.parseInt(PC_PARTS_AVAIL);
+			query.setInt(5, avilInt);
+			query.setString(6, PC_PARTS_DESC);
+			query.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
+			return 500;
+		}finally{
+			if(conn != null)
+				conn.close();
+		}
+		
+		return 200;
+	}
+	
 	public JSONArray queryReturnBrandParts(String brand) throws Exception{
 		PreparedStatement query = null;
 		Connection conn = null;
--- src/rest/inventory/V2_inventory.java
+++ src/rest/inventory/V2_inventory.java
@@ -1,6 +1,8 @@
 package rest.inventory;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -8,6 +10,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 
 import rest.dao.Schema;
@@ -76,4 +79,43 @@ public class V2_inventory {
 		}
 		return Response.ok(returnString).build();
 	}
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addPcParts(String incomingData) throws Exception{
+		String returnString = null;
+		Schema dao = new Schema();
+		
+		try{
+			System.out.println("incomingData: " + incomingData);
+			ObjectMapper mapper = new ObjectMapper();
+			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);
+			int httpCode =	dao.insertIntoPC_PARTS(itemEntry.PC_PARTS_TITLE, 
+					itemEntry.PC_PARTS_CODE, 
+					itemEntry.PC_PARTS_MAKER, 
+					itemEntry.PC_PARTS_AVAIL, 
+					itemEntry.PC_PARTS_DESC);
+			
+			if(httpCode == 200){
+				returnString = "Item inserted";
+			}
+			else{
+				return Response.status(500).entity("Unable to process Item").build();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+}
+
+class ItemEntry{
+	public String PC_PARTS_TITLE;
+	public String PC_PARTS_CODE;
+	public String PC_PARTS_MAKER;
+	public String PC_PARTS_AVAIL;
+	public String PC_PARTS_DESC;
 }
